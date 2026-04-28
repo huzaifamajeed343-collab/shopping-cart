9785953541e954641a057d9ee382cf674b1527df
huzaifa <huzaifamajeed343@gmail.com>
2026-04-28 19:01:15 +0400

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>We provide beautiful indoor plants to brighten your home.</p>
+    </div>
+  );
+}
